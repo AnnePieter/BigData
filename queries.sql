a6b3801ee395990ca3fb915b190275f3bebbf6f2
RSlomp <slom1500@student.nhl.nl>
2017-01-26 11:20:52 +0100

    Nieuwe SQL-vragen

--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
-SELECT naam
+SELECT t1.naam
 FROM Acteurs t1
-LEFT OUTER JOIN ActeursFilms
+LEFT OUTER JOIN ActeursFilms t2
+ON t1.ActeurID = t2.ActeurID
 WHERE t2.FilmID == t2.FilmID AND t2.ActeurId =! t2.ActeurID;
 
 SELECT COUNT FilmID
@@ -9,11 +10,30 @@ LEFT OUTER JOIN Acteurs t2
 ON t1.ActeurID = t2.ActeurID
 WHERE t2.naam = ;
 
-SELECT titel, MAX(buitenlandseOpbrangst)
-FROM Films;
+SELECT t1.titel
+FROM Films t1
+LEFT OUTER JOIN ActeursFilms t2
+ON t1.FilmID = t2.FilmID
+RIGHT OUTER JOIN Acteurs t3
+ON t2.ActeurID = t3.ActeurID
+WHERE t1.naam == "Joop Braakhekke";
 
-SELECT titel, MAX(beoordeling)
-FROM Films;
+SELECT titel
+FROM Films
+WHERE lengte = (SELECT MAX(lengte)
+				FROM Films
+				WHERE beoordeling >= 8.5);
 
-SELECT titel, MAX(productiekosten)
-FROM Films;
+SELECT titel
+FROM Films
+WHERE beoordeling = (SELECT MAX(beoordeling)
+					 FROM Films);
+
+SELECT t1.naam, t2.titel
+FROM Acteurs t1, Films t2
+LEFT OUTER JOIN ActeursFilms t2
+ON t1.FilmID = t2.FilmID
+RIGHT OUTER JOIN Acteurs t3
+ON t2.ActeurID = t3.ActeurID
+WHERE lengte = (SELECT MAX(lengte)
+				FROM Films);
