4ade23d877ea8df864c953699a574b66953840c8
Robert <robertbijl@live.nl>
2017-01-29 15:54:56 +0100

    Git commit

--- untitled/src/sample/Main.java
+++ untitled/src/sample/Main.java
@@ -1,5 +1,10 @@
 package sample;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
@@ -20,102 +25,129 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.DriverManager;
 import java.util.*;
 
 import javax.imageio.ImageIO;
+import javax.xml.transform.Result;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
 public class Main extends Application {
 
     Charset charset = StandardCharsets.ISO_8859_1;
-    File sourcefile = new File("C:///Users/annepieter/Documents/BigData/untitled/countries.csv");
-    Image img = new Image("file:///Users/annepieter/Documents/BigData/untitled/WorldMap.jpg");
-    String[] europe = {"Albania","Andorra","Armenia","Austria","Azerbaijan","Belarus","Belgium","Bosnia and Herzegovina","Bulgaria","Croatia","Cyprus","Czech Republic","Denmark","Estonia",
-            "Finland","France","Georgia","Germany","Greece","Hungary","Iceland","Ireland","Italy","Kazakhstan","Kosovo","Latvia","Liechtenstein","Lithuania","Luxembourg","Macedonia","Malta",
-            "Moldova","Monaco","Montenegro","Netherlands","Norway","Poland","Portugal","Romania","Russia","San Marino","Serbia","Slovakia","Slovenia","Spain","Sweden","Switzerland","Turkey","Ukraine","UK","Vatican City"};
+    File sourcefile = new File("D:///OneDrive/Documenten/school/Jaar2/Periode 2/Big Data/BigData/untitled/countries.csv");
+    Image img = new Image("file:///OneDrive/Documenten/school/Jaar2/Periode 2/Big Data/BigData/untitled/WorldMap.jpg");
+
+    String[] europe = {"Albania","Armenia","Austria","Azerbaijan","Belarus","Belgium","Bosnia and Herzegovina","Bulgaria","Croatia","Cyprus","Czech Republic","Denmark","Estonia",
+            "Finland","France","Georgia","Germany","Greece","Hungary","Ireland","Italy","Kazakhstan","Kosovo","Latvia","Lithuania","Luxembourg","Macedonia",
+            "Moldova","Netherlands","Norway","Poland","Portugal","Romania","Russia","Serbia","Slovakia","Slovenia","Spain","Sweden","Switzerland","Turkey","Ukraine","UK"};
+
+    GraphicsContext gc;
+    Map<String,Point2D.Double> coordsList;
+
+    Connection m_Connection;
+    Statement m_Statement;
+    ResultSet m_ResultSet;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //Database connection
+        GetDatabaseConnection();
 
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Group root1 = new Group();
-
-
         final Canvas canvas = new Canvas(2000,1500);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-
-        gc.drawImage(img,0,0,1920,1080);
-
         root1.getChildren().add(canvas);
-        //root1.getChildren().add(root);
 
+        gc = canvas.getGraphicsContext2D();
+        ResetGraphicsContext();
 
+        //Create scene
         Scene scene = new Scene(root1, 400,400);
         scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
         primaryStage.setFullScreen(true);
         primaryStage.setTitle("Visualisatie");
         primaryStage.setScene(scene);
-        Scanner scanner = new Scanner(sourcefile);
-        Map<String,Point2D.Double> coordsList = new HashMap<>();
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
 
-                String[] lineparts = line.split(",");
-                //
-                double xcoord = Double.parseDouble(lineparts[4]);
-                double ycoord = Double.parseDouble(lineparts[3]);
-                if(ycoord >920)
-                {
-                    ycoord -= 110;
-                }
-                else if(ycoord <920 && ycoord >720)
-                {
-                    ycoord -= 100;
-                }
-                else if(ycoord <720 && ycoord >540)
-                {
-                    ycoord -= 50;
-                }
-                coordsList.put(lineparts[2], new Point2D.Double(xcoord, ycoord));
-                gc.strokeOval(xcoord, ycoord,1,1);
-            }
+        //Set coords
+        SetCoordsToImage();
 
-            gc.strokeOval(620,870,5,5);
-        gc.strokeOval(1108,738,5,5);
+        Vraag_ImmigratieNederlanders();
+
+        primaryStage.show();
+    }
+
+    public void ResetGraphicsContext(){
+        gc.clearRect(0,0,1920,1080);
+        gc.drawImage(img,0,0,1920,1080);
+
+        gc.setFill(Color.BLUE);
+        gc.fillRect(10,10,300,300);
+    }
+
+    public void Vraag_ImmigratieNederlanders(){
+        ExecuteQuery("");
+
+        // Draw lines
         for(int x=0;x<europe.length;x++) {
 
             String euCountry = europe[x];
             gc.strokeLine(coordsList.get(euCountry).getX(), coordsList.get(euCountry).getY(), coordsList.get("USA").getX(), coordsList.get("USA").getY());
         }
-       /* gc.strokeOval(988,427,10,10);
-        gc.strokeOval(988,427,10,10);
-        gc.strokeOval(988,427,10,10);*/
-
-
-
-
-
-
-
-
-        primaryStage.show();
-
-        
-
     }
 
-    public static void main(String[] args) {
-        launch(args);
-
-
-
+    public void Vraag_2(){
+        ExecuteQuery("");
     }
 
-    public static void log(Object message){
-        System.out.println(String.valueOf(message));
+    public void GetDatabaseConnection(){
+        try{
+            Class.forName("org.postgresql.Driver");
+            m_Connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/BigData", "postgres", "admin");
+            m_Statement = m_Connection.createStatement();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
+    public void SetCoordsToImage() throws Exception{
+        Scanner scanner = new Scanner(sourcefile);
+        coordsList = new HashMap<>();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] lineparts = line.split(",");
+
+            double xCoord = Double.parseDouble(lineparts[4]);
+            double yCoord = Double.parseDouble(lineparts[3]);
+
+            if(yCoord >920)
+                yCoord -= 110;
+            else if(yCoord <920 && yCoord >720)
+                yCoord -= 100;
+            else if(yCoord <720 && yCoord >540)
+                yCoord -= 50;
+
+            coordsList.put(lineparts[2], new Point2D.Double(xCoord, yCoord));
+        }
+    }
+
+    public void ExecuteQuery(String query){
+        try{
+            m_ResultSet = m_Statement.executeQuery(query);
+
+            while (m_ResultSet.next()) {
+                System.out.println(m_ResultSet.getString(1) + ", " + m_ResultSet.getString(2) + ", "
+                        + m_ResultSet.getString(3));
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
 
 }
 
